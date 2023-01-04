92206000c718191b7d89630dc9660abe81c06e07
KjOHnBaggins <okenshieldak47@gmail.com>
2023-01-04 09:40:03 +0630

    postcreate output

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,6 @@ export default function Home() {
   );
 }
 
-// type PokemonFromServer = inferQueryResponse<getPokemonById>;
 type PokemonFromServer = PostCreateOutput;
 
 const PokemonListing: React.FC<{
